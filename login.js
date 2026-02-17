c6c5eccf124185e0e10300f4d027954bc6bb7951
Asraful Alam <asrafulspersonal@gmail.com>
2026-02-17 22:10:00 +0600

    make the Login Page Proper functional

---
+++ login.js
@@ -0,0 +1,22 @@
+// console.log('link js');
+
+document.getElementById('login-btn')
+        .addEventListener('click', function(){
+            // console.log('click success')
+            const numInput = document.getElementById('input-num');
+            const numValue = numInput.value;
+            // console.log(numValue)
+
+            const passInput = document.getElementById('input-pass');
+            const passValue = passInput.value;
+            // console.log(passValue);
+            
+            if(numValue === '01566006571' && passValue === '1234'){
+                alert('Login Successfully');
+                //  window.location.replace("/home.html");
+                window.location.assign('/home.html');
+            } else{
+                alert('Login Faild. Try Again');
+            }
+
+        })
